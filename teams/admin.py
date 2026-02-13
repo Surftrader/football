7ff288eca6e56c433162f179b882b66563d13e76
Sergiy Poddubka <poseal@gmail.com>
2026-02-13 11:44:00 +0200

    add admin panels for teams and users

--- teams/admin.py
+++ teams/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Team, Player, Match
 
-# Register your models here.
+
+class PlayerInline(admin.TabularInline):
+    model = Player
+    extra = 1
+    
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'city', 'manager', 'created_at')
+    search_fields = ('name', 'city')
+    list_filter = ('city',)
+    inlines = [PlayerInline]
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'position', 'number', 'is_injured')
+    search_fields = ('first_name', 'last_name')
+    list_filter = ('team', 'position', 'is_injured')
+
+@admin.register(Match)
+class MatchAdmin(admin.ModelAdmin):
+    list_display = ('home_team', 'away_team', 'match_date', 'home_score', 'away_score')
+    list_filter = ('match_date',)
+    date_hierarchy = 'match_date'
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'role', 'is_staff')
+    fieldsets = UserAdmin.fieldsets + (
+        ("Додаткова інформація", {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Додаткова інформація", {'fields': ('role',)}),
+    )
+    
