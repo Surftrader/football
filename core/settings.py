841444eecbe809b2b6de075c21b483a7178c3b5f
Sergiy Poddubka <poseal@gmail.com>
2026-02-18 19:28:13 +0200

    fix path redirect after registration

--- core/settings.py
+++ core/settings.py
@@ -128,6 +128,6 @@ STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-LOGIN_URL = 'login'
+LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'login'
+LOGOUT_REDIRECT_URL = '/auth/login/'
--- users/views.py
+++ users/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic import CreateView
 from django.contrib.auth import login
+from django.urls import reverse_lazy
 from .forms import ManagerRegistrationForm
 
 class RegisterView(CreateView):
     template_name = 'registration/register.html'
     form_class = ManagerRegistrationForm
-    success_url = '/login/'
+    success_url = reverse_lazy('login')
     
     def form_valid(self, form):
         response = super().form_valid(form)
